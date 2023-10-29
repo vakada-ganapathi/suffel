48a723758116cec4e50bd1affc1dc81ca4be0634
vakada-ganapathi <149322857+vakada-ganapathi@users.noreply.github.com>
2023-10-29 22:53:49 +0530

    Add files via upload

---
+++ Suffel.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Suffel {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+
+        
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+    }
+
+    public static void shuffleArray(int[] array) {
+        int n = array.length;
+        Random random = new Random();
+
+        for (int i = n - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
